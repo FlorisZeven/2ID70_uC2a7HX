cf4d41e15588fc9f818bbdaa56c260aa687a37cb
Thymen <thymen01@gmail.com>
2019-03-06 14:31:50 +0100

    Retry query 1 v3 (forgot to add %1,2%)

--- q2Queries.sql
+++ q2Queries.sql
@@ -1,4 +1,4 @@
-SELECT c.CourseName, Grade FROM  all_courses_passed as acp JOIN CourseOffers as co ON co.CourseOfferId = acp.CourseOfferId JOIN Courses as c ON c.CourseId = co.CourseId WHERE acp.StudentId = 3514485 AND acp.DegreeId = 5155 ORDER BY co.Year, co.Quartile, co.CourseOfferId;
+SELECT c.CourseName, Grade FROM  all_courses_passed as acp JOIN CourseOffers as co ON co.CourseOfferId = acp.CourseOfferId JOIN Courses as c ON c.CourseId = co.CourseId WHERE acp.StudentId = %1% AND acp.DegreeId = %2% ORDER BY co.Year, co.Quartile, co.CourseOfferId;
 SELECT 0;
 SELECT 0;
 SELECT 0;
