eb3e2f585a2b3d23d3d49ec0ca4e141b49c7cec5
Thymen <thymen01@gmail.com>
2019-03-06 10:04:10 +0100

    Removed Index on query 1

--- q2Create.sql
+++ q2Create.sql
@@ -8,9 +8,6 @@ CREATE MATERIALIZED VIEW all_courses_passed(StudentId, degreeId, CourseId, grade
      ORDER BY StudentId, DegreeId
 );
 
-CREATE INDEX acp ON (StudentId, degreeId, CourseId, grade);
-
-
 CREATE MATERIALIZED VIEW high_gpa(StudentRegistrationId, avg_grade) AS
 (
     SELECT StudentRegistrationId, avg(Grade)
