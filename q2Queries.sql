33e2062a3c966122672a32ead52f9e4414eb4313
Thymen <thymen01@gmail.com>
2019-03-07 19:10:29 +0100

    testing q4

--- q2Queries.sql
+++ q2Queries.sql
@@ -1,7 +1,7 @@
 SELECT c.CourseName, Grade FROM  all_courses_passed as acp JOIN CourseOffers as co ON co.CourseOfferId = acp.CourseOfferId JOIN Courses as c ON c.CourseId = co.CourseId WHERE acp.StudentId = %1% AND acp.DegreeId = %2% ORDER BY co.Year, co.Quartile, co.CourseOfferId;
 SELECT 0;
 SELECT DegreeId, cast(count(case when s.Gender = 'F' then 1 end) AS FLOAT)/ count(Gender) as Percentage FROM gpa_active_complete as gac JOIN Students as s ON gac.StudentId = s.StudentId WHERE gac.complete = 0 GROUP BY DegreeId ORDER BY DegreeId;;
-SELECT 0;
+SELECT Dept, cast(count(case when s.Gender = 'F' then 1 end) AS FLOAT)/ count(Gender) as Percentage FROM gpa_active_complete as gac  JOIN Students as s ON gac.StudentId = s.StudentId JOIN Degrees as d ON d.DegreeId = gac.DegreeId GROUP BY Dept HAVING Dept = %1%;
 SELECT 0;
 SELECT 0;
 SELECT 0;
